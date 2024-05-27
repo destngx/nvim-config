f435845113ccac08ae087c683e665a626943b8ac
destngx <chromevi123@gmail.com>
2024-05-27 09:19:17 +0700

    feat: render markdown

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -32,7 +32,7 @@ return {
     name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     config = function()
-      require('render-markdown').setup({})
+      require('plugins.markdown')
     end,
   },
   {
