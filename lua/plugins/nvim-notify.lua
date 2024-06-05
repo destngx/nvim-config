5f297ad676d49f5bf8b36485b9f30d3ea3d1171c
destngx <chromevi123@gmail.com>
2024-06-05 11:56:16 +0700

    feat: oil git status

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,4 +1,8 @@
-require("notify").setup({
+local nvim_notify = require("notify")
+
+nvim_notify.setup({
   background_colour = "#000000",
   render = "wrapped-compact",
 })
+
+vim.notify = nvim_notify
