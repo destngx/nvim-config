3b9bc3f469b48b6f96b78e1dbf5b98003e8bb219
destngx <chromevi123@gmail.com>
2024-06-06 10:48:00 +0700

    Killed history

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,16 @@
+require 'colorizer'.setup({
+  filetypes = {
+    'html',
+    'css',
+    'javascript',
+    'typescript',
+    'typescriptreact',
+    'javascriptreact',
+    'lua',
+    'python',
+  },
+  user_default_options = {
+    mode = "background",
+    tailwind = true, -- Enable tailwind colors
+  }
+})
