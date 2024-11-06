cac24d0ba6c94352651ac46ec3e762a77d7d6809
destngx <chromevi123@gmail.com>
2024-11-06 22:53:37 +0700

    fix: config dashboard

--- lua/plugins/appearance/dashboard.lua
+++ lua/plugins/appearance/dashboard.lua
@@ -1,4 +1,48 @@
 local icons = require("utils.icons")
+local function capture(cmd, raw)
+	local f = assert(io.popen(cmd, "r"))
+	local s = assert(f:read("*a"))
+	f:close()
+	if raw then
+		return s
+	end
+	s = string.gsub(s, "^%s+", "")
+	s = string.gsub(s, "%s+$", "")
+	s = string.gsub(s, "[\n\r]+", " ")
+	return s
+end
+local function split(source, sep)
+	local result, i = {}, 1
+	while true do
+		local a, b = source:find(sep)
+		if not a then
+			break
+		end
+		local candidat = source:sub(1, a - 1)
+		if candidat ~= "" then
+			result[i] = candidat
+		end
+		i = i + 1
+		source = source:sub(b + 1)
+	end
+	if source ~= "" then
+		result[i] = source
+	end
+	return result
+end
+-- if vim.fn.executable("onefetch") == 1 then
+  local header = split(
+    capture(
+      [[onefetch 2>/dev/null | sed 's/\x1B[@A-Z\\\]^_]\|\x1B\[[0-9:;<=>?]*[-!"#$%&'"'"'()*+,.\/]*[][\\@A-Z^_`a-z{|}~]//g']],
+      true
+    ),
+    "\n"
+  )
+  -- if next(header) ~= nil then
+  --   require("alpha.themes.dashboard").section.header.val = header
+  --   require("alpha").redraw()
+  -- end
+-- end
 
 return {
   'nvimdev/dashboard-nvim',
@@ -9,6 +53,7 @@ return {
       config = {
         week_header = {
           enable = true,
+          -- append = header,
         },
         shortcut = {
           { desc = icons.update .. 'Update',
