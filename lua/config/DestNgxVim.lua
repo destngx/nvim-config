b5d893d4e1f022d92dba711e0517db9354e05653
destngx <chromevi123@gmail.com>
2024-11-15 00:21:07 +0700

    fix: increase memory for typescript server

--- lua/config/DestNgxVim.lua
+++ lua/config/DestNgxVim.lua
@@ -20,7 +20,7 @@ DestNgxVim = {
     rooter = {
       -- Removing package.json from list in Monorepo Frontend Project can be helpful
       -- By that your live_grep will work related to whole project, not specific package
-      patterns = { ".git", "darcs", ".bzr", ".svn", "Makefile", ".eslintrc.js" }, -- Default
+      patterns = { ".git", "darcs", ".bzr", ".svn", "Makefile", ".eslintrc.js", "package.json" }, -- Default
     },
     -- <leader>z
     zen = {
