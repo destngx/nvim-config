0ce7aad4ef70eca195de21dc4454d59be55da239
destngx <chromevi123@gmail.com>
2024-11-16 09:39:37 +0700

    fix: config image view in markdown

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -11,16 +11,16 @@ return {
   {
     "3rd/image.nvim",
     enabled = os.getenv "IS_WSL" ~= "true",
-    ft = { "markdown" },
+    ft = { "markdown", "vimwiki" },
     dependencies = { "luarocks.nvim" },
     opts = {
       backend = "kitty",
       integrations = {
         markdown = {
           enabled = true,
-          clear_in_insert_mode = true,
+          clear_in_insert_mode = false,
           download_remote_images = true,
-          only_render_image_at_cursor = true,
+          only_render_image_at_cursor = false,
           filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
           resolve_image_path = function(document_path, image_path, fallback)
             -- document_path is the path to the file that contains the image
