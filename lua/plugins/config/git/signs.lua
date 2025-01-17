40e2b392aad017f0d2ff190f584e20f7c49f4741
destngx <chromevi123@gmail.com>
2025-01-17 22:28:07 +0700

    chore(sign): git sign column order high priority

--- lua/plugins/config/git/signs.lua
+++ lua/plugins/config/git/signs.lua
@@ -39,7 +39,7 @@ signs.setup {
     ignore_whitespace = false,
   },
   current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-  sign_priority                = 6,
+  sign_priority                = 16,
   update_debounce              = 100,
   status_formatter             = nil, -- Use default
   max_file_length              = 40000,
