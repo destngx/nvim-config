0ef13a4778b644d4a0ee6c718da3797d7811696a
destngx <chromevi123@gmail.com>
2025-04-08 22:23:01 +0700

    refactor(codecompanion): separate config and restructure config

---
+++ lua/plugins/config/codecompanion/adapters.lua
@@ -0,0 +1,13 @@
+return {
+  COPILOT = {
+    copilot = function()
+      return require("codecompanion.adapters").extend("copilot", {
+        schema = {
+          model = {
+            default = "claude-3.7-sonnet",
+          },
+        },
+      })
+    end,
+  }
+}
---
+++ lua/plugins/config/codecompanion/strategies.lua
@@ -0,0 +1,69 @@
+return {
+  chat = {
+    adapter = "copilot",
+    keymaps = {
+      send = {
+        modes = {
+          i = { "<C-CR>", "<C-s>" },
+        },
+      },
+      close = {
+        modes = {
+          n = "q",
+        },
+        index = 3,
+        callback = "keymaps.close",
+        description = "Close Chat",
+      },
+      stop = {
+        modes = {
+          n = "<C-c>",
+          i = "<C-c>",
+        },
+        index = 4,
+        callback = "keymaps.stop",
+        description = "Stop Request",
+      },
+      clear = {
+        modes = {
+          n = "gX",
+        },
+        index = 6,
+        callback = "keymaps.clear",
+        description = "Clear Chat",
+      },
+    },
+    slash_commands = {
+      ["help"] = {
+        opts = {
+          provider = "fzf_lua",     -- default|telescope|mini_pick|fzf_lua
+        },
+      },
+      ["symbols"] = {
+        opts = {
+          provider = "fzf_lua",     -- default|telescope|mini_pick|fzf_lua
+        },
+      },
+      ["buffer"] = {
+        opts = {
+          contains_code = true,
+          provider = "fzf_lua",     -- default|telescope|mini_pick|fzf_lua
+          has_params = true,
+        },
+      },
+      ["file"] = {
+        opts = {
+          contains_code = true,
+          max_lines = 1000,
+          provider = "fzf_lua",     -- telescope|mini_pick|fzf_lua
+        },
+      },
+    },
+  },
+  inline = {
+    adapter = "copilot",
+  },
+  agent = {
+    adapter = "copilot",
+  }
+}
