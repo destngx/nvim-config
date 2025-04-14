a628310bcb307d540e32b517892a12975b4aa66a
destngx <chromevi123@gmail.com>
2025-04-14 22:34:13 +0700

    feat(lsp): enhance diagnostic display with virtual lines and eslint code support

--- lua/lsp/config.lua
+++ lua/lsp/config.lua
@@ -123,6 +123,39 @@ vim.diagnostic.config({
   virtual_text = DestNgxVim.lsp.virtual_text and {
     prefix = DestNgxVim.icons.circle,
   } or false,
+  virtual_lines = {
+    enabled = true,
+    current_line = true,
+    severity = {
+      min = vim.diagnostic.severity.HINT,
+      max = vim.diagnostic.severity.ERROR,
+    },
+    format = function(diagnostic)
+      local code = diagnostic and diagnostic.user_data and diagnostic.user_data.lsp.code
+
+      if not diagnostic.source or not code then
+        return string.format('%s', diagnostic.message)
+      end
+
+      if diagnostic.source == 'eslint' then
+        for _, table in pairs(codes) do
+          if vim.tbl_contains(table, code) then
+            return string.format('%s [%s]', table.icon .. diagnostic.message, code)
+          end
+        end
+
+        return string.format('%s [%s]', diagnostic.message, code)
+      end
+
+      for _, table in pairs(codes) do
+        if vim.tbl_contains(table, code) then
+          return table.message
+        end
+      end
+
+      return string.format('%s [%s]', diagnostic.message, diagnostic.source)
+    end
+  }
 })
 -- Default Codelens command
 -- Each LSP client can override this function to provide custom codelens
