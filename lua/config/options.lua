7cccf6b88ad30318e72554fd4a96521e430dab2f
destngx <chromevi123@gmail.com>
2025-04-15 18:11:58 +0700

    style(ui): enhance visual appearance with custom fillchars and git conflict highlights
    - Add vertical line characters to fillchars for improved visual separation
    
    - Add custom highlight colors for git conflict markers
    
    - Format git conflict.lua file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -66,6 +66,8 @@ vim.opt.fillchars:append('fold: ');
 vim.opt.fillchars:append('foldopen: ');
 vim.opt.fillchars:append('foldsep: ');
 vim.opt.fillchars:append('foldclose:');
+vim.opt.fillchars:append('vert:▕');
+vim.opt.fillchars:append('vertleft:▕');
 
 for k, v in pairs(options) do
   vim.opt[k] = v
--- lua/plugins/config/git/conflict.lua
+++ lua/plugins/config/git/conflict.lua
@@ -1,21 +1,24 @@
 local present, conflict = pcall(require, "git-conflict")
 if not present then
-	return
+  return
 end
 
 conflict.setup({
-	default_mappings = true, -- disable buffer local mapping created by this plugin
-	default_commands = true, -- disable commands created by this plugin
-	disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
-	highlights = { -- They must have background color, otherwise the default color will be used
-		incoming = "DiffText",
-		current = "DiffAdd",
-	},
+  default_mappings = true,   -- disable buffer local mapping created by this plugin
+  default_commands = true,   -- disable commands created by this plugin
+  disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
+  highlights = {             -- They must have background color, otherwise the default color will be used
+    incoming = "DiffText",
+    current = "DiffAdd",
+  },
 })
 
+vim.api.nvim_set_hl(0, "GitConflictIncoming", { bg = "#293919" })
+vim.api.nvim_set_hl(0, "GitConflictIncomingLabel", { bold = true, bg = "#698F3F" })
+
 vim.api.nvim_create_autocmd("User", {
-	pattern = "GitConflictDetected",
-	callback = function()
-		vim.notify("Conflict detected in " .. vim.fn.expand("<afile>"))
-	end,
+  pattern = "GitConflictDetected",
+  callback = function()
+    vim.notify("Conflict detected in " .. vim.fn.expand("<afile>"))
+  end,
 })
