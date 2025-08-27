4866cccf133a6731f5e82ebab04d2ec7cecd1e04
destngx <chromevi123@gmail.com>
2025-08-27 16:12:45 +0700

    fix(lintint): check if eslint config available before lint

--- lua/plugins/config/linting.lua
+++ lua/plugins/config/linting.lua
@@ -55,11 +55,10 @@ local eslint_d = function(fname)
 end
 lint.linters_by_ft = {
   javascript = { eslint_d() },
-  -- javascript = { "eslint_d" },
-  typescript = { "eslint_d" },
-  javascriptreact = { "eslint_d" },
-  typescriptreact = { "eslint_d" },
-  svelte = { "eslint_d" },
+  typescript = { eslint_d() },
+  javascriptreact = { eslint_d() },
+  typescriptreact = { eslint_d() },
+  svelte = { eslint_d() },
   python = { "pylint" },
   makrdown = { "markdownlint-cli2", "vale" },
   dockerfile = { "hadolint" },
