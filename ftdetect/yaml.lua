50dcef6fb567a0c78067b39903890e0883788234
destngx <chromevi123@gmail.com>
2025-10-29 11:08:28 +0700

    fix(yaml): correct aclpolicy file extension pattern
    Update file pattern from `*.apolicy` to `*.aclpolicy` to properly detect
    ACL policy files as YAML filetype.

--- ftdetect/yaml.lua
+++ ftdetect/yaml.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	pattern = "*.yml,*.yaml,*.apolicy",
+	pattern = "*.yml,*.yaml,*.aclpolicy",
 	callback = function()
 		vim.bo.filetype = "yaml"
 	end,
