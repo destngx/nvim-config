eef37667fc22f1a03842ca5accb6812acce3bc26
destngx <chromevi123@gmail.com>
2026-01-02 10:14:54 +0700

    fix: ssh yank with tmux using osc 52 clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -93,3 +93,17 @@ end
 for k, v in pairs(globals) do
   vim.g[k] = v
 end
+
+if os.getenv("SSH_TTY") then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+end
