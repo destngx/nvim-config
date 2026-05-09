7ee64ce1a0a19e4b38cc062eb7c1a7be1cf6275c
destngx <chromevi123@gmail.com>
2026-05-09 19:00:24 +0700

    fix: update obsidian plugin removed deprecated

--- lua/plugins/languages/markdown_file.lua
+++ lua/plugins/languages/markdown_file.lua
@@ -122,6 +122,7 @@ return {
     },
     config = function()
       require("obsidian").setup({
+        legacy_commands = false,
         workspaces = {
           {
             name = "Persona",
@@ -158,41 +159,35 @@ return {
           time_format = "%H:%m",
         },
         attachments = {
-          img_folder = "_attachments",
-          img_text_func = function(client, path)
-            path = client:vault_relative_path(path) or path
-            return string.format("![[%s]]", path.name)
+          folder = "_attachments",
+          img_text_func = function(path)
+            local name = vim.fn.fnamemodify(path, ":t")
+            return string.format("![[%s]]", name)
           end,
         },
         note_id_func = function(title)
-          -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-          -- In this case a note with the title 'My new note' will be given an ID that looks
-          -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
           local suffix = ""
           if title ~= nil then
-            -- If title is given, transform it into valid file name.
             suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
           else
-            -- If title is nil, just add 4 random uppercase letters to the suffix.
             for _ = 1, 4 do
               suffix = suffix .. string.char(math.random(65, 90))
             end
           end
           return tostring(os.time()) .. "-" .. suffix
         end,
-        disable_frontmatter = false,
-        note_frontmatter_func = function(note)
-          -- This is equivalent to the default frontmatter function.
-          local out = { tags = note.tags }
-          -- `note.metadata` contains any manually added fields in the frontmatter.
-          -- So here we just make sure those fields are kept in the frontmatter.
-          if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-            for k, v in pairs(note.metadata) do
-              out[k] = v
+        frontmatter = {
+          enabled = false,
+          func = function(note)
+            local out = { tags = note.tags }
+            if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+              for k, v in pairs(note.metadata) do
+                out[k] = v
+              end
             end
-          end
-          return out
-        end,
+            return out
+          end,
+        },
       })
     end,
   },
